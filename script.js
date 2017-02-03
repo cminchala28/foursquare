e11886f4ab112d206eb2b0174b3b5d05187f89c6
cminchala28 <cminchala@cristoreyny.org>
2017-02-03 13:35:48 +0000

    final

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // JavaScript File
-$(document).ready(){
-$("#submit") .click(function(){
+$(document).ready(function(){
+$("#Submit") .click(function(){
   var url= "https://api.foursquare.com/v2/venues/search"
-  var client= "?client_id=CLIENT_ID"
-   var secret= "&client_secret=CLIENT_SECRET"
+  var client= "?client_id=MRBSX3VVOXTRIQ3D0WGUVZLW1X2PFMNWYUKFDRGX4NT1GLX5"
+   var secret= "&client_secret=5JXQTSWKP1CADWPMDJUZ4WTX53K4O30UNRQHG1QVPEXQKGTN"
   var v= "&v=20130815"
-  var l= "&ll=40.7,-74"
-  var query= "&query=sushi"
-  varfullUrl= url+ client + secret + v + l + query
-    })                                                                                                                                                                                                                                                                                                                                                                   
-})
+  var l= "&near=" + $("#location").val()
+  var query= "&query=" + $("#type").val()
+  var fullUrl= url+ client + secret + v + l + query
+    console.log(fullUrl)
+    $.getJSON(fullUrl,function(response){
+   
+    });
+  
+  });
+    
+});
